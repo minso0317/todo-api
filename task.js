427536f6c5b6c4accc2ba7e9a8f9f58d49c02819
MINSOO SHIN <minso0317@naver.com>
2025-02-13 09:21:40 +0900

    validate 타이틀 최소 단어수 지정

--- task.js
+++ task.js
@@ -6,6 +6,12 @@ const TaskSchema = new mongoose.Schema(
       type: String,
       required: true,
       maxLength: 30,
+      validate: {
+        validator: function (title) {
+          return title.split(" ").length > 1; // 한 칸 공백을 설정해줘서 두 단어 이상으로 지정
+        },
+        message: () => "Must be at least 2 words", // 최소 타이틀 단어 수 지정
+      },
     },
     description: {
       type: String,
